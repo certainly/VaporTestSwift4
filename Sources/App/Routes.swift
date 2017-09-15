f348d1721a25a44b6346e09f92c3c5e1821fc673
certainly <j1o2b3@gmail.com>
2017-09-15 16:23:26 +0800

    ~ test for ubuntu

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -1,16 +1,18 @@
 import Vapor
+import  Foundation
 
 extension Droplet {
     
     struct Foo: Codable {
         let dd: String
+
     }
     
     
     func setupRoutes() throws {
         get("hello") { req in
             var json = JSON()
-            try json.set("hello", "world")
+            try json.set("hel3lo", "world")
             return json
         }
 
@@ -25,7 +27,20 @@ extension Droplet {
         }
 
         get("description") { req in return req.description }
-        
+
+        test()
         try resource("posts", PostController.self)
+    }
+
+    func test() {
+        print("dfd")
+        let encoder = JSONEncoder()
+        let data = Foo(dd: "df")
+        do {
+            try encoder.encode(data)
+        } catch {
+            print(error)
+        }
+        print("okk")
     }
 }
